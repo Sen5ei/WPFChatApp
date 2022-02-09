d2226957c8758bc566ff0b9ba79c6ef7096ab10c
Ognjen Sredic <nsredic@sbb.rs>
2022-02-10 00:53:00 +0100

    Login Page ViewModel Binding

---
+++ WPFChatApp/AttachedProperties/ButtonAttachedProperty.cs
@@ -0,0 +1,10 @@
+﻿namespace WPFChatApp
+{
+    /// <summary>
+    /// The IsBusy attached property  for anything that wants to flag if the control is busy
+    /// </summary>
+    public class IsBusyProperty : BaseAttachedProperty<IsBusyProperty, bool>
+    {
+
+    }
+}
