b6dad0ad09433bb38bfafa2cf434ce366be18d08
Ognjen Sredic <nsredic@sbb.rs>
2022-02-15 00:11:29 +0100

    Add Chat List ItemsControl

---
+++ WPFChatApp/ViewModel/Chat/ChatListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace WPFChatApp
+{
+    /// <summary>
+    /// A view model fore the overview chat list
+    /// </summary>
+    public class ChatListViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// The chat list items for the list
+        /// </summary>
+        public List<ChatListItemViewModel> Items { get; set; }
+    }
+}
