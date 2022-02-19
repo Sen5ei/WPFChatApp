6047a80362df4d234a465dae325c036fb4aef370
Ognjen Sredic <nsredic@sbb.rs>
2022-02-19 01:22:43 +0100

    Complete the page navigation

--- WPFChatApp/Pages/BasePage.cs
+++ WPFChatApp/Pages/BasePage.cs
@@ -8,20 +8,10 @@ using WPFChatApp.Core;
 namespace WPFChatApp
 {
     /// <summary>
-    /// A base page for all pages to gain base functionality
+    /// The base page for all pages to gain base functionality
     /// </summary>
-    public class BasePage<VM> : Page
-        where VM : BaseViewModel, new()
+    public class BasePage : Page
     {
-        #region Private Member
-
-        /// <summary>
-        /// The View Model associated with this page
-        /// </summary>
-        private VM mViewModel;
-
-        #endregion
-
         #region Public Properties
 
         /// <summary>
@@ -37,27 +27,13 @@ namespace WPFChatApp
         /// <summary>
         /// The time any slide animation takes to complete
         /// </summary>
-        public float SlideSeconds { get; set; } = 0.8f;
+        public float SlideSeconds { get; set; } = 0.4f;
 
         /// <summary>
-        /// The View Model associated with this page
+        /// A flag to indicate if this page should animate out on load
+        /// Useful for when we are moving the page to another frame
         /// </summary>
-        public VM ViewModel
-        {
-            get => mViewModel;
-            set
-            {
-                // If nothing has changed, return
-                if (mViewModel == value)
-                    return;
-
-                // Update the value
-                mViewModel = value;
-
-                // Set the data context for this page
-                DataContext = mViewModel;
-            }
-        }
+        public bool ShouldAnimateOut { get; set; }
 
         #endregion
 
@@ -73,10 +49,7 @@ namespace WPFChatApp
                 Visibility = Visibility.Collapsed;
 
             // Listen out for the page loading
-            Loaded += Async;
-
-            // Create a default view model
-            ViewModel = new VM();
+            Loaded += BasePage_LoadedAsync;
         }
 
         #endregion
@@ -88,10 +61,16 @@ namespace WPFChatApp
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private async void Async(object sender, System.Windows.RoutedEventArgs e)
+        private async void BasePage_LoadedAsync(object sender, System.Windows.RoutedEventArgs e)
         {
-            // Animate the page in
-            await AnimateInAsync();
+            // If we are setup to animate out on load
+            if (ShouldAnimateOut)
+                // Animate out the page
+                await AnimateOutAsync();
+            // Otherwise...
+            else
+                // Animate the page in
+                await AnimateInAsync();
         }
 
         /// <summary>
@@ -136,6 +115,59 @@ namespace WPFChatApp
 
                     break;
             }
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// A base page with added VieWModel support
+    /// </summary>
+    public class BasePage<VM> : BasePage
+        where VM : BaseViewModel, new()
+    {
+        #region Private Member
+
+        /// <summary>
+        /// The View Model associated with this page
+        /// </summary>
+        private VM mViewModel;
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// The View Model associated with this page
+        /// </summary>
+        public VM ViewModel
+        {
+            get => mViewModel;
+            set
+            {
+                // If nothing has changed, return
+                if (mViewModel == value)
+                    return;
+
+                // Update the value
+                mViewModel = value;
+
+                // Set the data context for this page
+                DataContext = mViewModel;
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public BasePage() : base()
+        {
+            // Create a default view model
+            ViewModel = new VM();
         }
 
         #endregion
--- WPFChatApp/Pages/RegisterPage.xaml.cs
+++ WPFChatApp/Pages/RegisterPage.xaml.cs
@@ -1,18 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Security;
 using WPFChatApp.Core;
 
 namespace WPFChatApp
@@ -20,7 +6,7 @@ namespace WPFChatApp
     /// <summary>
     /// Interaction logic for RegisterPage.xaml
     /// </summary>
-    public partial class RegisterPage : BasePage<LoginViewModel>, IHavePassword
+    public partial class RegisterPage : BasePage<RegisterViewModel>, IHavePassword
     {
         public RegisterPage()
         {
