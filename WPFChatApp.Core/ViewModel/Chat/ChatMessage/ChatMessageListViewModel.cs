3dd5188e634057b78e230f07259698045e091a06
Ognjen Sredic <nsredic@sbb.rs>
2022-02-22 21:53:06 +0100

    Style Send message chat bubble

---
+++ WPFChatApp.Core/ViewModel/Chat/ChatMessage/ChatMessageListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace WPFChatApp.Core
+{
+    /// <summary>
+    /// A view model fore a chat messgae thread list
+    /// </summary>
+    public class ChatMessageListViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// The chat thread items for the list
+        /// </summary>
+        public List<ChatMessageListItemViewModel> Items { get; set; }
+    }
+}
