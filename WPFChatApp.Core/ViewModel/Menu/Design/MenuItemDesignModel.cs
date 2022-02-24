880fef8a2dc4f5e2b49f9ed729acfb3f0abcbadd
Ognjen Sredic <nsredic@sbb.rs>
2022-02-24 18:38:20 +0100

    Create Attachment Menu popup

---
+++ WPFChatApp.Core/ViewModel/Menu/Design/MenuItemDesignModel.cs
@@ -0,0 +1,30 @@
+﻿namespace WPFChatApp.Core
+{
+    /// <summary>
+    /// The design-time data for a <see cref="MenuItemViewModel"/>
+    /// </summary>
+    public class MenuItemDesignModel : MenuItemViewModel
+    {
+        #region Singleton
+
+        /// <summary>
+        /// A single instance of the design model
+        /// </summary>
+        public static MenuItemDesignModel Instance => new MenuItemDesignModel();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public MenuItemDesignModel()
+        {
+            Text = "Hello world";
+            Icon = IconType.File;
+        }
+
+        #endregion
+    }
+}
--- WPFChatApp.Core/ViewModel/PopupMenu/ChatAttachmentPopupMenuViewModel.cs
+++ WPFChatApp.Core/ViewModel/PopupMenu/ChatAttachmentPopupMenuViewModel.cs
@@ -1,11 +1,12 @@
-﻿using WPFChatApp.Core;
+﻿using System.Collections.Generic;
+using WPFChatApp.Core;
 
 namespace WPFChatApp
 {
     /// <summary>
     /// A view model for any popup menus
     /// </summary>
-    public class ChatAttachmentPopupMenuViewModel : BasePopupMenuViewModel
+    public class ChatAttachmentPopupMenuViewModel : BasePopupViewModel
     {
         #region Public Properties
 
@@ -18,7 +19,15 @@ namespace WPFChatApp
         /// </summary>
         public ChatAttachmentPopupMenuViewModel()
         {
-
+            Content = new MenuViewModel
+            {
+                Items = new List<MenuItemViewModel>(new[]
+                {
+                    new MenuItemViewModel { Text = "Attach a file...", Type = MenuItemType.Header },
+                    new MenuItemViewModel { Text = "From Computer", Icon = IconType.File },
+                    new MenuItemViewModel { Text = "From Pictures", Icon = IconType.Picture },
+                })
+            };
         }
 
         #endregion
