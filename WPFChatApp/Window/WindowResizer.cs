472e1bd884f90c9ed00049d1a176859acd03ea62
Ognjen Sredic <nsredic@sbb.rs>
2022-02-26 01:41:28 +0100

    Fix system menu cursor position when maximized on second monitor

--- WPFChatApp/Window/WindowResizer.cs
+++ WPFChatApp/Window/WindowResizer.cs
@@ -115,6 +115,13 @@ namespace WPFChatApp
         /// </summary>
         public Rectangle CurrentMonitorSize { get; set; } = new Rectangle();
 
+        /// <summary>
+        /// The size and position of the current screen in relation to the multi-screen desktop
+        /// For example a second monitor on the right will have a Left position of
+        /// the X resolution of the screens on the left
+        /// </summary>
+        public Rect CurrentScreenSize => mScreenSize;
+
         #endregion
 
         #region Constructor
@@ -361,6 +368,19 @@ namespace WPFChatApp
 
             // Now we have the max size, allow the host to tweak as needed
             Marshal.StructureToPtr(lMmi, lParam, true);
+        }
+
+        /// <summary>
+        /// Gets the current cursor position in screen coordinates relative to an entire multi-desktop position
+        /// </summary>
+        /// <returns></returns>
+        public Point GetCursorPosition()
+        {
+            // Get mouse position
+            GetCursorPos(out POINT lMousePosition);
+
+            // Apply DPI scaling
+            return new Point(lMousePosition.X / mMonitorDpi.Value.DpiScaleX, lMousePosition.Y / mMonitorDpi.Value.DpiScaleY);
         }
     }
 
