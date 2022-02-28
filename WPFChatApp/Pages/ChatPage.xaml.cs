426fc2a433dbf5321529b6879d2be8f43137626f
Ognjen Sredic <nsredic@sbb.rs>
2022-02-28 15:02:43 +0100

    Create navigation to specific Chat pages

--- WPFChatApp/Pages/ChatPage.xaml.cs
+++ WPFChatApp/Pages/ChatPage.xaml.cs
@@ -1,19 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using WPFChatApp.Core;
+﻿using WPFChatApp.Core;
 
 namespace WPFChatApp
 {
@@ -22,10 +7,21 @@ namespace WPFChatApp
     /// </summary>
     public partial class ChatPage : BasePage<ChatMessageListViewModel>
     {
-        public ChatPage()
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public ChatPage() : base()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Constructor with specific view model
+        /// </summary>
+        /// <param name="specificViewModel">The specific view model to use for this page</param>
+        public ChatPage(ChatMessageListViewModel specificViewModel) : base(specificViewModel)
+        {
+            InitializeComponent();
+        }
     }
 }
